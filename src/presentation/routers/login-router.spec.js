07f8ddd627627c882ae44d38c9a0f6cd2db0156f
ThomasDantas <thomas@paipe.co>
2021-02-17 15:33:29 -0300

    ensure LoginRouter returns 400 if passowrd is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,7 +2,7 @@
 
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -16,6 +16,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'anyway'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'anyway@gmail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
