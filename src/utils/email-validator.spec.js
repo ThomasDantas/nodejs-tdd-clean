fa17a47c3f34afe14a9509f723c9c817da2eeeec
ThomasDantas <thomas@paipe.co>
2021-02-23 10:32:18 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -11,18 +11,22 @@ const makeSut = () => {
 }
 
 describe('Email Validator', () => {
-  test('should return true if validator returns true', () => {
+  test('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
-})
 
-describe('Email Validator', () => {
-  test('should return true if validator returns false', () => {
+  test('Should return true if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
