0bfe6bbd75c34012ff22c0b296e750047386ada1
ThomasDantas <thomas@paipe.co>
2021-03-03 20:24:28 -0300

    ensure TokenGenerator call JWT with correct values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,7 +1,11 @@
 module.exports = {
   token: 'any_token',
+  id: '',
+  secret: '',
 
-  sign (id, scret) {
+  sign (id, secret) {
+    this.id = id
+    this.secret = secret
     return this.token
   }
 }
