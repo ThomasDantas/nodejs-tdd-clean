b9567569614e6610371ebc68516092fd52876ab6
ThomasDantas <thomas@paipe.co>
2021-03-04 21:28:16 -0300

    move sute creation to a factory helper method

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,20 +1,25 @@
 const { MongoClient } = require('mongodb')
 
 let client, db
-
-const makeSut = () => {
-  class LoadUserByEmailRepository {
-    constructor (userModel) {
-      this.userModel = userModel
-    }
-
-    async load (email) {
-      const user = this.userModel.findOne({ email })
-      return user
-    }
+class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
   }
 
-  return new LoadUserByEmailRepository()
+  async load (email) {
+    const user = this.userModel.findOne({ email })
+    return user
+  }
+}
+
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new LoadUserByEmailRepository(userModel)
+
+  return {
+    userModel,
+    sut
+  }
 }
 
 describe('LoadUserByEmail Repository', () => {
@@ -35,18 +40,16 @@ describe('LoadUserByEmail Repository', () => {
   })
 
   test('Should return null if no user is found', async () => {
-    const userModel = db.collection('users')
-    const sut = makeSut(userModel)
+    const { sut } = makeSut()
     const user = await sut.load('invalid_email@mail.com')
     expect(user).toBeNull()
   })
 
   test('Should return an user if user is found', async () => {
-    const userModel = db.collection('users')
+    const { sut, userModel } = makeSut()
     await userModel.insertOne({
       email: 'valid_email@mail.com'
     })
-    const sut = makeSut(userModel)
     const user = await sut.load('valid_email@mail.com')
     expect(user.email).toBe('valid_email@mail.com')
   })
