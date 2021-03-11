99f276ffbc0fe3ec3103c737bc67c1f8d4da1c9b
ThomasDantas <thomas@paipe.co>
2021-03-11 15:44:43 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -12,4 +12,17 @@ describe('App Setup', () => {
 
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('hello')
+    })
+
+    const res = await request(app)
+      .get('/test')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
